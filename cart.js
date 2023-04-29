c9d7f27b7e32cb592939f94ddce8cfc5b9e6145a
mctoy87 <mctoy87@mail.ru>
2023-04-29 12:39:09 +0200

    cart.js was debugged with ESLint

--- cart.js
+++ cart.js
@@ -2,46 +2,49 @@
 
 
 const cart = {
-    items: [],      //это товары
-    
+  items: [], // это товары
 
-    get totalPrice() {                  //общая стоимость корзины получается через геттер
-        return this.calculateItemPrice();
-    },
+  get totalPrice() { // общая стоимость корзины получается через геттер
+    return this.calculateItemPrice();
+  },
 
-    count: 0,       //количество товаров
+  count: 0, // количество товаров
 
-    add (productName, price, currentCount = 1) {    //добавить товар   
-        this.items.push({ productName, price, currentCount });                   //этот метод формирует объект из полученных параметров и добавляет его в свойство items
-        this.increaseCount(currentCount);           //так же вызывает все необходимые методы чтобы свойства count и totalPrice были актуальные
-        // this.calculateItemPrice();
-    },
+  add(productName, price, currentCount = 1) { // добавить товар
+    // метод формирует объект из параметров и добавляет его в свойство items
+    this.items.push({productName, price, currentCount});
+    // вызывает все методы чтобы свойства count и totalPrice были актуальные
+    this.increaseCount(currentCount);
+    // this.calculateItemPrice();
+  },
 
-    calculateItemPrice() {                                  //посчитать общую стоимость товаров.
-        return this.items.reduce((acc, item) => {  
-            let total = acc + item.price * item.currentCount;    //пересчитывает стоимость всей корзины используя метод reduce и записывает значение в totalPrice
-            return total; 
-        }, 0);
-    },
+  calculateItemPrice() { // посчитать общую стоимость товаров.
+    return this.items.reduce((acc, item) => {
+    // пересчитывает стоимость всей корзины reduce и записывает в totalPrice
+      const total = acc + item.price * item.currentCount;
+      return total;
+    }, 0);
+  },
 
-    increaseCount (number) {                        //увеличить количество товаров. Принимает один параметр(число). Увеличивает свойство count на это число
-        this.count += number;
-        return this.count;
-    },
+  // увеличить количество товаров (count) на параметр.
+  increaseCount(number) {
+    this.count += number;
+    return this.count;
+  },
 
-    clear() {                                       //Очищает полностью нашу корзину, возвращает все значения в изначальные
-        this.items = [];
+  clear() {// Очищает полностью корзину, возвращает все значения в изначальные
+    this.items = [];
 
-        this.count = 0;
-    },
+    this.count = 0;
+  },
 
-    print() {
-        console.log(JSON.stringify(this.items));
-        console.log(JSON.stringify(this.totalPrice));
-    }
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(JSON.stringify(this.totalPrice));
+  },
+};
 
-}
-cart.add('Огурец', 200)
+cart.add('Огурец', 200);
 console.log(cart.totalPrice);
 console.log(cart.count);
 
@@ -55,16 +58,16 @@ console.log(cart.count);
 
 cart.clear();
 // console.log(cart);
-console.log (cart.totalPrice);
+console.log(cart.totalPrice);
 
 cart.print();
 console.log(cart);
 
 // можно обозначить объект еще так
 
-// const objName = {                           //add() Принимает три параметра: 1.название товара, 2.цену товара, 3.количество товара (опциональный параметр, по умолчанию 1 товар)
-//     productName,
+// add() Принимает три параметра: название, цену, количество  по умол. 1 товар)
+// const objName = {
 //     price,
 //     currentCount,
-// };                         
-// this.items.push(objName);  
+// };
+// this.items.push(objName);
